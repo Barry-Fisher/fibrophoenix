6781fddc774d44769a5b2c8bcdfe49e6afc6fa1f
Barry Fisher <barry.fisher@reallifedigital.com>
2018-08-11 17:02:54 +0100

    Small var rename

--- middleware/validPaths.js
+++ middleware/validPaths.js
@@ -2,7 +2,7 @@ export default async function ({ route, app }) {
   const checkForCollection = async function (routeName, collection) {
     if (route.name === routeName && Object.keys(route.params).length > 0) {
       const allItems = await app.$content.getCollectionData(collection)
-      const validPaths = allItems.map(article => article.path)
+      const validPaths = allItems.map(item => item.path)
       if (!validPaths.includes(route.path)) {
         app.router.app.error({ statusCode: 404, message: 'This page could not be found' })
       }
